8109b9d0a439e5155994214100feb0c075ab219f
javeshmonga <javeshmonga@ccs.neu.edu>
2018-06-14 21:46:07 -0400

    getting data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as http from 'http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  queryTerm: String;
+  Foods = [];
+  food;
+
+  selectSearch() {
+    console.log(this.queryTerm);
+    this.service()
+      .then(Foods => {
+        this.Foods = Foods;
+      });
+    console.log(this.Foods);
+  }
+
+  service() {
+    return fetch('http://localhost:3000/api/poc/' + this.queryTerm)
+      .then(response => response.json());
+  }
+
+  displayDetails(food) {
+    console.log(food.food_name);
+  }
 }
